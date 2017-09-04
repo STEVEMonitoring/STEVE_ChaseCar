7bedc86878ccca0811c79c9c9f5d74bbae98ae21
K-G Eriksson <kge013@live.se>
2017-09-04 23:35:00 +0200

    Started to add functionality for extracting data values from the data field in the CAN frames. Also added test project.

---
+++ STEVE_Chase_Car/STEVE_Chase_Car/Code/DataField.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEVE_Chase_Car.Code
+{
+    public class DataField
+    {
+        private const uint BITS_PER_BYTE = 8;
+        private byte[] data = new byte[8];
+
+        public DataField(byte[] data)
+        {
+            Array.Copy(data, this.data, this.data.Length);
+        }
+        public bool get_bit(char offset)
+        {
+            throw new NotImplementedException("get_bit not implemented yet!");
+        }
+
+        public uint get_value(uint offset, uint width)
+        {
+            uint offsetInByte = offset % BITS_PER_BYTE;
+            uint bytesToProcess = 1 + width / BITS_PER_BYTE;
+
+            byte[] dataBytes = new byte[8];
+            Array.Copy(this.data, offset / BITS_PER_BYTE, dataBytes, 0, 8 - offset / BITS_PER_BYTE);
+
+            uint retVal = (uint)dataBytes[0] >> (byte)offsetInByte;
+
+            byte bytesProcessed = 1;
+
+            for (; bytesProcessed < bytesToProcess; ++bytesProcessed)
+            {
+                retVal +=
+                    (uint)(dataBytes[bytesProcessed] << (byte)(BITS_PER_BYTE * bytesProcessed))
+                    >> (byte)offsetInByte;
+            }
+
+            // Mask upper unused bits in hightes byte
+            return (uint)(retVal & ~(0xFFFF << (int)width));
+        }
+    }
+}
---
+++ STEVE_Chase_Car/Tests_STEVE_Chase_Car/UnitTest_DataField.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using STEVE_Chase_Car.Code;
+
+namespace Tests_STEVE_Chase_Car
+{
+    [TestClass]
+    public class UnitTest_DataField
+    {
+        [TestMethod]
+        public void ConstructorGenerateDefaultInstance()
+        {
+            Assert.IsNotNull(new DataField(new byte[8]));
+        }
+
+        [TestMethod]
+        public void ReturnsFirstByte()
+        {
+            Assert.AreEqual<uint>(
+                0x42,
+                (new DataField(new byte[8] {0x42, 0, 0, 0, 0, 0, 0, 0 }).get_value(0, 8))
+                );
+        }
+    }
+}
