53c6cf0ea4d7ce1a1c09cf0780310c96c00ec399
Fredrik Carlsson <fredrik.carlsson.92@gmail.com>
2018-01-12 11:17:47 +0100

    Changed layout of chart view buttons and screen

--- STEVE_Chase_Car/STEVE_Chase_Car/FormsWindows/ChartView.Designer.cs
+++ STEVE_Chase_Car/STEVE_Chase_Car/FormsWindows/ChartView.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.databaseChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.sTEVE_DatabaseDataSet = new STEVE_Chase_Car.STEVE_DatabaseDataSet();
             this.sTEVEDatabaseDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
@@ -53,17 +53,18 @@
             this.databaseChart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            chartArea1.Name = "ChartArea1";
-            this.databaseChart.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.databaseChart.Legends.Add(legend1);
-            this.databaseChart.Location = new System.Drawing.Point(12, 69);
+            chartArea2.Name = "ChartArea1";
+            this.databaseChart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.databaseChart.Legends.Add(legend2);
+            this.databaseChart.Location = new System.Drawing.Point(16, 85);
+            this.databaseChart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.databaseChart.Name = "databaseChart";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.databaseChart.Series.Add(series1);
-            this.databaseChart.Size = new System.Drawing.Size(616, 460);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.databaseChart.Series.Add(series2);
+            this.databaseChart.Size = new System.Drawing.Size(821, 566);
             this.databaseChart.TabIndex = 0;
             this.databaseChart.Text = "chart1";
             this.databaseChart.Click += new System.EventHandler(this.databaseChart_Click);
@@ -90,42 +91,58 @@
             // 
             // btnBmsPdo1
             // 
-            this.btnBmsPdo1.Location = new System.Drawing.Point(12, 27);
+            this.btnBmsPdo1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(45)))), ((int)(((byte)(60)))));
+            this.btnBmsPdo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBmsPdo1.ForeColor = System.Drawing.Color.White;
+            this.btnBmsPdo1.Location = new System.Drawing.Point(16, 33);
+            this.btnBmsPdo1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBmsPdo1.Name = "btnBmsPdo1";
-            this.btnBmsPdo1.Size = new System.Drawing.Size(75, 23);
+            this.btnBmsPdo1.Size = new System.Drawing.Size(100, 28);
             this.btnBmsPdo1.TabIndex = 5;
             this.btnBmsPdo1.Text = "BMS PDO1";
-            this.btnBmsPdo1.UseVisualStyleBackColor = true;
+            this.btnBmsPdo1.UseVisualStyleBackColor = false;
             this.btnBmsPdo1.Click += new System.EventHandler(this.btnBmsPdo1_Click);
             // 
             // btnBmsPdo2
             // 
-            this.btnBmsPdo2.Location = new System.Drawing.Point(169, 27);
+            this.btnBmsPdo2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(45)))), ((int)(((byte)(60)))));
+            this.btnBmsPdo2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBmsPdo2.ForeColor = System.Drawing.Color.White;
+            this.btnBmsPdo2.Location = new System.Drawing.Point(225, 33);
+            this.btnBmsPdo2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBmsPdo2.Name = "btnBmsPdo2";
-            this.btnBmsPdo2.Size = new System.Drawing.Size(75, 23);
+            this.btnBmsPdo2.Size = new System.Drawing.Size(100, 28);
             this.btnBmsPdo2.TabIndex = 6;
             this.btnBmsPdo2.Text = "BMS PDO2";
-            this.btnBmsPdo2.UseVisualStyleBackColor = true;
+            this.btnBmsPdo2.UseVisualStyleBackColor = false;
             this.btnBmsPdo2.Click += new System.EventHandler(this.btnBmsPdo2_Click);
             // 
             // btnMotorFrame0
             // 
-            this.btnMotorFrame0.Location = new System.Drawing.Point(340, 27);
+            this.btnMotorFrame0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(45)))), ((int)(((byte)(60)))));
+            this.btnMotorFrame0.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMotorFrame0.ForeColor = System.Drawing.Color.White;
+            this.btnMotorFrame0.Location = new System.Drawing.Point(453, 33);
+            this.btnMotorFrame0.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnMotorFrame0.Name = "btnMotorFrame0";
-            this.btnMotorFrame0.Size = new System.Drawing.Size(100, 23);
+            this.btnMotorFrame0.Size = new System.Drawing.Size(133, 28);
             this.btnMotorFrame0.TabIndex = 7;
             this.btnMotorFrame0.Text = "Motor Frame 0";
-            this.btnMotorFrame0.UseVisualStyleBackColor = true;
+            this.btnMotorFrame0.UseVisualStyleBackColor = false;
             this.btnMotorFrame0.Click += new System.EventHandler(this.btnMotorFrame0_Click);
             // 
             // btnMotorFrame1
             // 
-            this.btnMotorFrame1.Location = new System.Drawing.Point(534, 27);
+            this.btnMotorFrame1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(45)))), ((int)(((byte)(60)))));
+            this.btnMotorFrame1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMotorFrame1.ForeColor = System.Drawing.Color.White;
+            this.btnMotorFrame1.Location = new System.Drawing.Point(712, 33);
+            this.btnMotorFrame1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnMotorFrame1.Name = "btnMotorFrame1";
-            this.btnMotorFrame1.Size = new System.Drawing.Size(94, 23);
+            this.btnMotorFrame1.Size = new System.Drawing.Size(125, 28);
             this.btnMotorFrame1.TabIndex = 8;
             this.btnMotorFrame1.Text = "Motor Frame 1";
-            this.btnMotorFrame1.UseVisualStyleBackColor = true;
+            this.btnMotorFrame1.UseVisualStyleBackColor = false;
             this.btnMotorFrame1.Click += new System.EventHandler(this.btnMotorFrame1_Click);
             // 
             // clbChartChoice
@@ -134,23 +151,26 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.clbChartChoice.CheckOnClick = true;
             this.clbChartChoice.FormattingEnabled = true;
-            this.clbChartChoice.Location = new System.Drawing.Point(634, 69);
+            this.clbChartChoice.Location = new System.Drawing.Point(845, 85);
+            this.clbChartChoice.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.clbChartChoice.Name = "clbChartChoice";
-            this.clbChartChoice.Size = new System.Drawing.Size(188, 214);
+            this.clbChartChoice.Size = new System.Drawing.Size(249, 259);
             this.clbChartChoice.TabIndex = 9;
             this.clbChartChoice.MouseUp += new System.Windows.Forms.MouseEventHandler(this.clbChartChoice_MouseUp);
             // 
             // ChartView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(834, 541);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1112, 666);
             this.Controls.Add(this.clbChartChoice);
             this.Controls.Add(this.btnMotorFrame1);
             this.Controls.Add(this.btnMotorFrame0);
             this.Controls.Add(this.btnBmsPdo2);
             this.Controls.Add(this.btnBmsPdo1);
             this.Controls.Add(this.databaseChart);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ChartView";
             this.Text = "ChartView";
             ((System.ComponentModel.ISupportInitialize)(this.databaseChart)).EndInit();
--- STEVE_Chase_Car/STEVE_Chase_Car/FormsWindows/DatabaseRawView.Designer.cs
+++ STEVE_Chase_Car/STEVE_Chase_Car/FormsWindows/DatabaseRawView.Designer.cs
@@ -45,9 +45,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.steveDatabaseView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.steveDatabaseView.Location = new System.Drawing.Point(12, 12);
+            this.steveDatabaseView.Location = new System.Drawing.Point(16, 15);
+            this.steveDatabaseView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.steveDatabaseView.Name = "steveDatabaseView";
-            this.steveDatabaseView.Size = new System.Drawing.Size(639, 438);
+            this.steveDatabaseView.Size = new System.Drawing.Size(852, 539);
             this.steveDatabaseView.TabIndex = 0;
             // 
             // sTEVE_DatabaseDataSet
@@ -63,9 +64,10 @@
             // btnLoadTable
             // 
             this.btnLoadTable.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnLoadTable.Location = new System.Drawing.Point(534, 526);
+            this.btnLoadTable.Location = new System.Drawing.Point(712, 647);
+            this.btnLoadTable.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLoadTable.Name = "btnLoadTable";
-            this.btnLoadTable.Size = new System.Drawing.Size(117, 41);
+            this.btnLoadTable.Size = new System.Drawing.Size(156, 50);
             this.btnLoadTable.TabIndex = 1;
             this.btnLoadTable.Text = "Load Table";
             this.btnLoadTable.UseVisualStyleBackColor = true;
@@ -82,21 +84,23 @@
             "MotorFrame1",
             "MotorFrame2",
             "MPPT"});
-            this.CBsourceSelect.Location = new System.Drawing.Point(12, 456);
+            this.CBsourceSelect.Location = new System.Drawing.Point(16, 561);
+            this.CBsourceSelect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CBsourceSelect.Name = "CBsourceSelect";
-            this.CBsourceSelect.Size = new System.Drawing.Size(164, 21);
+            this.CBsourceSelect.Size = new System.Drawing.Size(217, 24);
             this.CBsourceSelect.TabIndex = 2;
             this.CBsourceSelect.Text = "System.Data.DataViewManagerListItemTypeDescriptor";
             // 
             // DatabaseRawView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(663, 579);
+            this.ClientSize = new System.Drawing.Size(884, 713);
             this.Controls.Add(this.CBsourceSelect);
             this.Controls.Add(this.btnLoadTable);
             this.Controls.Add(this.steveDatabaseView);
-            this.MinimumSize = new System.Drawing.Size(679, 618);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(899, 750);
             this.Name = "DatabaseRawView";
             this.Text = "DatabaseRawView";
             this.Load += new System.EventHandler(this.DatabaseRawView_Load);
